91ccc4d5500ad0295fe464053765c97aa389a969
Luis Antonio Velasco <antonioluis6.velasco@gmail.com>
2025-07-20 22:22:22 -0600

    Actualizado config seguro y agregado .env para evitar vulnerabilidades

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'any-secret-key'
-    MONGO_URI = 'mongodb+srv://cripto:1@cluster0.kgtnxvp.mongodb.net/artedix?retryWrites=true&w=majority'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'any-secret-key')  # valor por defecto si no lo defines
+    MONGO_URI = os.getenv('MONGO_URI')
+
     MONGODB_SETTINGS = {
         'db': 'artedix',
         'host': MONGO_URI
