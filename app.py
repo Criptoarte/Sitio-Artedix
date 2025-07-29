806983c1f228dc953968466a9c657ad131d24d72
Luis Antonio Velasco <antonioluis6.velasco@gmail.com>
2025-07-28 22:00:54 -0600

    versión con app.py adaptado

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
+import os
+
+import flask
+from flask.json.provider import DefaultJSONProvider
+flask.json.JSONEncoder = DefaultJSONProvider
+
 from flask import Flask
 from config import Config
 from app_routes import bp as routes_bp
 from models import db
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})
+# Configuración CORS global
+CORS(app, resources={
+    r"/*": {
+        "origins": "*",
+        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
 
 db.init_app(app)
+
 app.register_blueprint(routes_bp)
 
 if __name__ == '__main__':
